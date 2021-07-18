332b6820c2f005d65e9afe703a80e645de64a75f
racio <85686582+RacioTrajano@users.noreply.github.com>
2021-07-18 16:20:11 +0000

    quase lá

--- main.c
+++ main.c
@@ -1,23 +1,31 @@
 #include<stdio.h>
 #include<string.h>
 
-void posicao(char* text, char* padrao){
+void posicao(int tam_text, int tam_padrao, char* text, char* padrao)
+{
 
-int tam_text =strlen(text);
-int tam_padrao=strlen(padrao);
+
+int fim =0;
 
 
 for (int i=0; i<=tam_text - tam_padrao; i++){
+  if (fim == 0){
   for(int j=0; j<tam_padrao; j++){
-    while(text[i+j]== padrao[j]){
-      printf("%c", text[i+j]);
-
+    if(text[i+j]== padrao[j] || j != tam_padrao-1){
+      printf("%c ", text[i+j]);
+    }
+    if(text[i+j] != padrao[j]){
+      printf("%c não", text[i+j]);
+      break;
     }
     if(j==tam_padrao-1){
-      printf("Achei o padrão no íncie: %d", i);
+      printf("sim \nAchei o padrão no índice %d", i);
+      fim =1;
+      break;
     }
   }
   printf("\n");
+}
 }
 
 
@@ -27,14 +35,17 @@ for (int i=0; i<=tam_text - tam_padrao; i++){
 
 int main() {
 
-char text[] = "AAAABAAA";
-char chave[]= "BAAA";
 
-char* substring;
+char text[50] ={0};
+char padrao[5]= {0};
 
+scanf(" %s", text);
+scanf(" %s", padrao);
 
+int tam_text =strlen(text);
+int tam_padrao=strlen(padrao);
 
+posicao(tam_text,tam_padrao, text,padrao);
 
-
-  return 0;
+ return 0;
 }
